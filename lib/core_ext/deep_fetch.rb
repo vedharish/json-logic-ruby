862aac7cd04a570b09056c982c99bee0e10d888f
Harish Ved <ved.harish3@gmail.com>
2018-01-30 09:37:49 +0530

    Needed dig method to move to ruby 2.2.5

--- lib/core_ext/deep_fetch.rb
+++ lib/core_ext/deep_fetch.rb
@@ -3,11 +3,23 @@ class Hash
     keys = key.to_s.split('.')
     dig(*keys) || default rescue default
   end
+
+  def dig(key, *args)
+    value = self[key]
+    return value if args.length == 0 || value.nil?
+    value.dig(*args)
+  end
 end
 
 class Array
   def deep_fetch(index, default = nil)
     indexes = index.to_s.split('.').map(&:to_i)
     dig(*indexes) || default rescue default
+  end
+
+  def dig(key, *args)
+    value = self.at(key)
+    return value if args.length == 0 || value.nil?
+    value.dig(*args)
   end
 end
